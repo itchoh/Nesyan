a5cb6c6cc0e5ad56c5297b718cadf005eccbe553
YaraZalat <yarazalat6@gmail.com>
2025-12-14 06:46:15 +0200

    font style

---
+++ lib/constantText/text_style.dart
@@ -0,0 +1,205 @@
+
+import 'package:flutter/material.dart';
+
+class AppFontStyle{
+  static TextStyle Title1(Color c){
+    return TextStyle(
+        color: c,
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Inter'
+    );
+  }
+  static TextStyle Lists1(){
+    return const TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.w300,
+        fontFamily: 'Inter'
+    );
+  }
+  static TextStyle TitleGame(){
+    return const TextStyle(
+        color: Colors.black38,
+        fontSize: 18,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Inter'
+    );
+  }
+  static TextStyle PhoneNumber(){
+    return const TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Inter'
+    );
+  }
+  static TextStyle Alert(){
+    return const TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Inter'
+    );
+  }
+  static TextStyle TimeOrSimiButton(){
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+      fontFamily: 'Inter'
+    );
+  }
+  static TextStyle TitleInputText(){
+    return const TextStyle(
+      fontSize: 23,
+      color: Colors.black,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500
+
+    );
+  }
+  static TextStyle Gender(){
+    return const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: Colors.black,
+    );
+  }
+  static TextStyle TitleShowBottomSheetWidgetTit(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 25,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle Quection(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle NumQues(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 26,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle NameOrtextinnnOrSecondTitleOrThirdTitle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle Butt(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 40,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle DoubleButt(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 23,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle TextInInputField(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle AnswerOrTitCardORreminderItemsOrInputButtomSheet(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle TextinnnOrSecondTitle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle TitlePage(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 22,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle Subtitle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle TitleItemsButtomSheetOritemsReminder(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle disease(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle TitlePageBold(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 22,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle buttont(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle HintText(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 11,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Inter',
+    );
+  }
+  static TextStyle MessageBoxTitle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Inter',
+    );
+  }
+
+}
