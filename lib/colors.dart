5589473582f5fb43a9d39302f544969d995e18be
hesham <metwally820@gmail.com>
2025-12-14 05:14:52 +0200

    Add colors.dart

---
+++ lib/colors.dart
@@ -0,0 +1,83 @@
+
+import 'package:flutter/material.dart';
+
+const white = Colors.white;
+const coolBlack = Color(0xFF101623);
+const traditionalGreen = Color(0xFF4CAF50);
+const black = Color(0xFF000000);
+const grayChateau = Color(0xFFA1A8B0);
+
+const classicMatteGrey = Color(0xFF6E6E6E);
+const grannyApple = Color(0xFFD1FAE5);
+const whiteMarble = Color(0xFFE5E7EB);
+const luxuryWhite = Color(0xFFF9FAFB);
+const brownMadder = Color(0xFFE1282B);
+const trueBlack = Color(0xFF0A0A0A);
+const superGrey = Color(0xFF9A9A9A);
+const sweetGrey = Color(0xFFD9D9D9);
+const whiteGray = Color(0xFFF3F3F5);
+const taurus = Color(0xFF2E7D32);
+const papayaWhip = Color(0xFFFFEDD4);
+const rainbowGrey = Color(0xFFC5C5C5);
+const modelUltramarine = Color(0xFF0F469A);
+const darkCharcoal = Color(0xFF343333);
+const shockingRed = Color(0xFFF41717);
+const heather = Color(0xFF717784);
+const techBlue = Color(0xFF2979EE);
+const sportyPurple = Color(0xFF9B14FA);
+const crystalPeak = Color(0xFFDBEAFE);
+const blueChalk = Color(0xFFF3E8FF);
+const techWhite = Color(0xFFF5F5F5);
+const darkGreenTurquoise = Color(0xFF13B97D);
+const comet = Color(0xFF232B43);
+const matteGraphite = Color(0xFF424242);
+const livid = Color(0xFF639ED5);
+const saintBartholomew = Color(0xFFA02525);
+const matteGoldenrod = Color(0xFFAF9B4C);
+
+const simpleLightBlue = Color(0xFFD1ECFA);
+const warmYellowCream = Color(0xFFFDF5CA);
+const redShimmer = Color(0xFFFF5C5C);
+const brightGray = Color(0xFF3B4453);
+const chalice = Color(0xFFADADAD);
+
+final sweetGrey50 = const Color(0xFFD9D9D9).withOpacity(0.5);
+
+final black05 = const Color(0xFF000000).withOpacity(0.05);
+final black10 = const Color(0xFF000000).withOpacity(0.1);
+final black15 = const Color(0xFF000000).withOpacity(0.15);
+final black20 = const Color(0xFF000000).withOpacity(0.2);
+final black30 = const Color(0xFF000000).withOpacity(0.3);
+final black35 = const Color(0xFF000000).withOpacity(0.35);
+final black44 = const Color(0xFF000000).withOpacity(0.44);
+final black50 = const Color(0xFF000000).withOpacity(0.5);
+final black60 = const Color(0xFF000000).withOpacity(0.6);
+final black70 = const Color(0xFF000000).withOpacity(0.7);
+final black80 = const Color(0xFF000000).withOpacity(0.8);
+
+final gin40 = const Color(0xFFE8F5E9).withOpacity(0.4);
+final gin50 = const Color(0xFFE8F5E9).withOpacity(0.5);
+final gin60 = const Color(0xFFE8F5E9).withOpacity(0.6);
+
+final neutralOffWhite10 = const Color(0xFFE8E6E6).withOpacity(0.1);
+final traditionalGreen92 = const Color(0xFF4CAF50).withOpacity(0.92);
+
+final cuteGrey80 = const Color(0xFFB3B3B3).withOpacity(0.8);
+final starDust50 = const Color(0xFF9E9E9E).withOpacity(0.5);
+final starDust70 = const Color(0xFF9E9E9E).withOpacity(0.7);
+
+final brownMadder54 = const Color(0xFFE1282B).withOpacity(0.54);
+final brownMadder80 = const Color(0xFFE1282B).withOpacity(0.8);
+final brownMadder90 = const Color(0xFFE1282B).withOpacity(0.9);
+
+final white20 = const Color(0xFFFFFFFF).withOpacity(0.2);
+final white80 = const Color(0xFFFFFFFF).withOpacity(0.8);
+final white95 = const Color(0xFFFFFFFF).withOpacity(0.95);
+
+final padua35 = const Color(0xFFABE2C6).withOpacity(0.35);
+final pinkDiamond35 = const Color(0xFFE2ABAB).withOpacity(0.35);
+final shockingRed10 = const Color(0xFFF41717).withOpacity(0.1);
+
+final simpleGreen40 = const Color(0xFF159F33).withOpacity(0.4);
+final traditionalGreen40 = const Color(0xFF4CAF50).withOpacity(0.4);
+final digitalRed75 = const Color(0xFFFF0000).withOpacity(0.75);
