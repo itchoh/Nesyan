7e4b55d2290dd862abce420d993c08d54410935e
YaraZalat <yarazalat6@gmail.com>
2025-12-15 01:32:23 +0200

    icon

---
+++ lib/core/constant/icons.dart
@@ -0,0 +1,32 @@
+class AppIcons {
+
+  static const String play = 'assets/icons/play-button.png';
+  static const String profile = 'assets/icons/profile.png';
+  static const String reminder = 'assets/icons/reminder.png';
+  static const String restaurant = 'assets/icons/restaurant.png';
+  static const String sun = 'assets/icons/sun.png';
+  static const String time = 'assets/icons/time.png';
+  static const String visibility = 'assets/icons/visibility.png';
+  static const String watch = 'assets/icons/watch.png';
+  static const String age = 'assets/icons/age.png';
+  static const String alarm = 'assets/icons/alarm.png';
+  static const String analysis = 'assets/icons/analysis.png';
+  static const String camera = 'assets/icons/camera.png';
+  static const String capsule = 'assets/icons/capsule.png';
+  static const String chat = 'assets/icons/chat.png';
+  static const String check = 'assets/icons/check.png';
+  static const String checkmark = 'assets/icons/checkmark.png';
+  static const String coffee = 'assets/icons/coffee.png';
+  static const String delete = 'assets/icons/delete.png';
+  static const String edit = 'assets/icons/edit.png';
+  static const String family = 'assets/icons/family.png';
+  static const String game = 'assets/icons/game.png';
+  static const String gender = 'assets/icons/gender-identity.png';
+  static const String home = 'assets/icons/home.png';
+  static const String idCard = 'assets/icons/id-card.png';
+  static const String location = 'assets/icons/location.png';
+  static const String lock = 'assets/icons/lock.png';
+  static const String logout = 'assets/icons/logout.png';
+  static const String mail = 'assets/icons/mail.png';
+  static const String mic = 'assets/icons/mic.png';
+}
